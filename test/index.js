d28249c49c7482a957f13d50a7233dd2a9a8a2a9
Masaaki Morishita <ikasama3.0@gmail.com>
2015-07-26 12:34:16 +0900

    fix

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
 var test = require('tape')
 var border = require('..')
 
-test('description', function (t) {
+test('parse', function (t) {
   var actual = border.parse('1px solid #eee')
   var expected = [ { width: '1px' }, { style: 'solid' }, { color: '#eee' } ]
   t.same(actual, expected)
   t.end()
 })
 
-test('description', function (t) {
+test('stringify', function (t) {
   var actual = border.stringify([
     {width: '1px'},
     {style: 'solid'},
