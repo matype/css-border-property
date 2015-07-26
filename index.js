42eb9e7dac34880bf8f596747e5b7174b51e58c9
Masaaki Morishita <ikasama3.0@gmail.com>
2015-07-26 12:21:48 +0900

    first implementation

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-module.exports = function () {
+var isColor = require('is-color')
+var isLength = require('is-css-length')
+var isStyle = require('is-border-style')
+
+function check (val) {
+  if (isColor(val)) return 'color'
+  if (isLength(val)) return 'width'
+  if (isStyle(val)) return 'style'
+
+  return null
+}
+
+module.exports.parse = function (str) {
+  var value = str.split(' ')
+  var ret = []
+  value.forEach(function (v) {
+    var prop = check(v)
+    if (prop) {
+      var obj = {}
+      obj[prop] = v
+      ret.push(obj)
+    }
+  })
+  return ret
+}
+
+module.exports.stringify = function (arr) {
+  var prop = []
+  arr.forEach(function (val) {
+    Object.keys(val).forEach(function (key) {
+      prop.push(val[key])
+    })
+  })
+  return prop.join(' ')
 }
--- test/index.js
+++ test/index.js
@@ -1,6 +1,16 @@
 var test = require('tape')
-var css-border-property = require('..')
+var border = require('..')
 
 test('description', function (t) {
-    t.end()
+  border.parse('1px solid #eee')
+  t.end()
+})
+
+test('description', function (t) {
+  border.stringify([
+    {width: '1px'},
+    {style: 'solid'},
+    {color: '#fff'},
+  ])
+  t.end()
 })
